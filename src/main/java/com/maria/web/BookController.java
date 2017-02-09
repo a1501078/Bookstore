2948474302a09dec926424c1daba129cabd5d45d
Kuikka Maria <a1501078@myy.haaga-helia.fi>
2017-02-09 17:04:46 +0200

    Chapter 5.1

--- src/main/java/com/maria/web/BookController.java
+++ src/main/java/com/maria/web/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.maria.domain.Book;
 import com.maria.domain.BookRepository;
@@ -46,6 +47,17 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     } 
+	//REST all books
+	@RequestMapping(value="books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	//REST book by id
+	@RequestMapping(value="book/{id}", method=RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findOne(bookId);
+	}
 	
 	  @RequestMapping(value = "/add")
 	    public String addStudent(Model model){
